3d12fd0b12b580f50ddd7dfd940bc74ce07cd903
ningh4 <ningh4@mcmaster.ca>
2021-10-31 11:40:13 -0400

    add maps and index bg img

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+var images = new Array(
+    './img/img1.jpeg',
+    './img/img2.jpeg',
+    './img/img3.jpeg',
+    './img/img4.jpeg',
+    './img/img5.jpeg',
+    './img/img6.jpeg',
+    './img/img7.jpeg',
+    './img/img8.jpeg',
+    './img/img9.jpeg',
+    './img/img10.jpeg',
+    './img/img11.jpeg',
+    './img/img12.jpeg',
+    './img/img13.jpeg',
+    './img/img14.jpeg',
+    './img/img15.jpeg',
+    './img/img16.jpeg',
+    './img/img17.jpeg'
+);
+
+var slider = setInterval(function() {
+    document.getElementsByClassName('backImg')[0].setAttribute('style', 'background-image: url("'+images[0]+'")');
+    images.splice(images.length, 0, images[0]);
+    images.splice(0, 1);
+}, 4000);
+
---
+++ js/individual_sample.js
@@ -0,0 +1,49 @@
+// Initialize and add the map
+function initMap() {
+
+	// Hardcoded the location of Aga Khan Museum in latitude and longitude 
+	var aga = { lat: 43.726, lng: -79.332 };
+
+	// Create a map centered at Aga Khan Museum location
+	var map = new google.maps.Map(document.getElementById("map"), {
+		// Specify zoom level
+		zoom: 14,
+		center: aga,
+		// Restrict the map to a particular lat and long bounds
+		restriction: {
+		    latLngBounds: {
+				north: 60,
+				south: 20,
+				east: -60,
+				west: -100,
+		    },
+		},
+	});
+
+	// Create a marker positioned at the museum 
+	var marker = new google.maps.Marker({
+		position: aga,
+		map: map,
+	});
+	
+	// Aga Khan Museum Info Window content
+	var aga_info = '<div id="content">' + 
+		'<h1 id="firstHeading" class="firstHeading">Aga Khan Museum</h1>' +
+		'<div id="bodyContent">' + 
+		'<p>A museum of Islamic art, Iranian art and Muslim culture</p>' +
+		'<p>Rated 4.6<i class="material-icons" style="font-size:16px;">star</i></p>' + 
+		'</div>' +
+		'</div>'
+	;
+
+	// Create InfoWindows for the museum
+	var aga_info_window = new google.maps.InfoWindow({
+		content: aga_info
+	});
+
+	// Create a click listener for the marker so that when users click it, a label appears
+	marker.addListener('click', function() {
+		aga_info_window.open(map, marker);
+	});
+	
+}
