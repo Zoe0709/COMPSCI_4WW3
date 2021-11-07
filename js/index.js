21175e0aad3485798da2587da2352b9a5c08b9d2
ningh4 <ningh4@mcmaster.ca>
2021-11-06 23:49:29 -0400

    nav bar and foorter animate

--- js/index.js
+++ js/index.js
@@ -26,3 +26,19 @@ var slider = setInterval(function() {
     images.splice(0, 1);
 }, 4000);
 
+// Function that displays content of the dropdown
+function dropthebox() {
+    document.getElementById("userdropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+
+
