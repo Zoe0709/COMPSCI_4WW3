8d28e24ef2c9f41f19d94ffb28c702020b0e1bb5
abdo <realabdullah403@gmail.com>
2021-11-10 23:16:08 -0500

    Changes to the index page images

--- js/index.js
+++ js/index.js
@@ -1,29 +1,13 @@
 // Create an array containing all background images
-var images = new Array(
-    './img/img1.jpeg',
-    './img/img2.jpeg',
-    './img/img3.jpeg',
-    './img/img4.jpeg',
-    './img/img5.jpeg',
-    './img/img6.jpeg',
-    './img/img7.jpeg',
-    './img/img8.jpeg',
-    './img/img9.jpeg',
-    './img/img10.jpeg',
-    './img/img11.jpeg',
-    './img/img12.jpeg',
-    './img/img13.jpeg',
-    './img/img14.jpeg',
-    './img/img15.jpeg',
-    './img/img16.jpeg',
-    './img/img17.jpeg'
-);
+
+var count = 1;
 
 // Function that changes background image repeatedly every 4 seconds
 var slider = setInterval(function() {
-    document.getElementsByClassName('backImg')[0].setAttribute('style', 'background-image: url("'+images[0]+'")');
-    images.splice(images.length, 0, images[0]);
-    images.splice(0, 1);
+
+    nextImg = './img/img' + count + '.jpeg';
+    document.getElementsByClassName('backImg')[0].setAttribute('style', 'background-image: url("' + nextImg + '")');
+    count = (count + 1) % (4);
 }, 4000);
 
 // Function that displays content of the dropdown
@@ -34,11 +18,9 @@ function dropthebox() {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
+        var myDropdown = document.getElementById("myDropdown");
         if (myDropdown.classList.contains('show')) {
             myDropdown.classList.remove('show');
         }
     }
 }
-
-
